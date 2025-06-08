34453c84f41da1bceac8ad4c2173ece8689843e1
Sumitmakhija2507 <sumitmakhija2507@gmail.com>
2025-06-08 15:42:27 +0530

    new

--- src/shared/Component/coming-soon/page.tsx
+++ src/shared/Component/coming-soon/page.tsx
@@ -1,28 +1,28 @@
 export default async function CommingSoon() {
 
-  return (
-      <>
-          <div className="exchange-wrapper">
-              <div className="container">
-                  <div className="row justify-content-center gap-md-0 gap-3">
-                      <div className="col-7">
-                          <div className="card shadow-sm">
-                              <div className="card-body p-4">
-                                  <div className="d-flex justify-content-center mb-3">
-                                      <span className="card-title p-1">In the Works!</span>
-                                  </div>
-                                  <div className="inner-card w-100 py-2 px-3 mt-3 text-center" style={{minHeight: 350}}>
-                                      <div className="w-100 mx-auto" style={{ maxWidth: 600, margin: '100px 0'}}>
-                                          <h1 className="display-4">🚀 Coming Soon!</h1>
-                                          <p className="lead">We're working hard to bring something awesome. Stay tuned!</p>
-                                      </div>
-                                  </div>
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-              </div>
-          </div>
-      </>
-  );
+    return (
+        <>
+            <div className="exchange-wrapper">
+                <div className="container">
+                    <div className="row justify-content-center gap-md-0 gap-3">
+                        <div className="col-7">
+                            <div className="card shadow-sm">
+                                <div className="card-body p-4">
+                                    <div className="d-flex justify-content-center mb-3">
+                                        <span className="card-title p-1">In the Works!</span>
+                                    </div>
+                                    <div className="inner-card w-100 py-2 px-3 mt-3 text-center" style={{ minHeight: 350 }}>
+                                        <div className="w-100 mx-auto" style={{ maxWidth: 600, margin: '100px 0' }}>
+                                            <h1 className="display-4">🚀 Coming Soon!</h1>
+                                            <p className="lead">We&apos;re working hard to bring something awesome. Stay tuned!</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 }
